d34f41d0bb0327752d8b1f32933f718761582678
omkarponde <pondeomkar16@gmail.com>
2024-05-16 01:02:07 +0530

    Updated main.py

--- app/main.py
+++ app/main.py
@@ -34,12 +34,12 @@ def create_log_directory():
 async def read_root():
     return {"message": "Hello, FastAPI!!!"}
 
-if __name__ == "__main__":
-    try:
-        create_log_directory()
-    except:
-        print("exception occured")
-    # import uvicorn
+# if __name__ == "__main__":
+#     try:
+#         create_log_directory()
+#     except:
+#         print("exception occured")
+#     # import uvicorn
     # uvicorn.run(app, host="0.0.0.0", port=8000)
     # port = int(os.getenv("PORT", 8000))
     # uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True)
